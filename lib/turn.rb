fccb1efb50cfba5161d81fad5c6678ecae86ff3d
tj11 Johnston <tedwardj11@gmail.com>
2018-04-12 16:33:54 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,5 @@
 def display_board(board)
-  puts ""
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def valid_move?(board,index)
