4f7c7df5dc29b3cd11f7621c2e2f48f8f3776703
tj11 Johnston <tedwardj11@gmail.com>
2018-04-12 16:35:03 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,6 +2,9 @@ def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index
+  gets
+
 def valid_move?(board,index)
   if index.between(0,9) == true
     return true
